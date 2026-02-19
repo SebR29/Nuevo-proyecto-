fb21eaf772dcc823c5d28e30cdee1f8bc1fcbaee
dianaosorio <dianaosorio32osorio@gmail.com>
2026-02-19 14:35:53 -0500

    hola

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("DIANA VALENTINA OSORIO LASSO");
+        System.out.println("escuchar musica,leer,montar bici");
+        System.out.println("Atletismo");
 
 
 
 
         }
     }
-    
