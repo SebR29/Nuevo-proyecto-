9959aa6f40ab799863e04352ec10b49d79a8efb0
Seb <rudas42299@gmail.com>
2026-02-19 14:24:07 -0500

    Nombre , Hobbys y Deporte

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        System.out.println("Luis Sebastian Rudas Burgos");
+        System.out.println("Mis hobbys son: Jugar vieojuegos, patinar y dibujar");
+        System.out.println("si pratico deporte es Voleyball");
 
 
         }
